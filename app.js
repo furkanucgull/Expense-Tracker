eb52e0a87ce390cd4e48b811eccc0e781ad114a4
furkanucgull <furkanucgull@gmail.com>
2024-06-20 12:38:12 +0200

    AppDone

--- app.js
+++ app.js
@@ -3,73 +3,84 @@ const btnAdd = document.querySelector('.btn-add');
 const date = document.querySelector('.date');
 const amountInput = document.querySelector('.amount');
 const total = document.getElementById('total');
-const table = document.querySelector('.table-list');
+const table = document.querySelector('.table-list tbody');
 
 let expenses = JSON.parse(localStorage.getItem('expenses')) || [];
-let totalAmount = 0;
+
 displayExpenses();
 sumExpenses();
-//let totalAmount = expenses.reduce((sum, expense) => sum + expense.amountValue, 0);
-if (expenses === null) {
-  expenses = {
-    categoryValue: 0,
-    amountValue: 0,
-    dateValue: 0,
-  };
-}
+
 function addExpense() {
-  btnAdd.addEventListener('click', () => {
+  btnAdd.addEventListener('click', e => {
+    e.preventDefault();
+
     let categoryValue = category.value;
-    const amountValue = Number(amountInput.value);
+    let amountValue = Number(amountInput.value);
     let dateValue = date.value;
-    if (dateValue === '' || amountValue === '') {
-      alert('Please fill the requirement sections!');
+
+    if (dateValue === '' || isNaN(amountValue) || amountValue <= 0) {
+      alert('Please fill the required sections with valid values!');
       return;
     }
+
     expenses.push({ categoryValue, amountValue, dateValue });
-    totalAmount += amountValue;
-
     localStorage.setItem('expenses', JSON.stringify(expenses));
+    amountInput.value = '';
 
-    const html = `  <tr>
-    <th class="table-info">${categoryValue}</th>
-    <th class="table-info">${amountValue}</th>
-    <th class="table-info">${dateValue}</th>
-    <td id="td-delete"><button class="delete-btn">Delete</button></td>
-    </tr>`;
-    total.textContent = `Total : ${totalAmount} $`;
-    table.innerHTML += html;
-    sumExpenses();
-  });
-}
-function displayExpenses() {
-  expenses.forEach((expense, index) => {
-    const { categoryValue, amountValue, dateValue } = expense;
     const html = `
-    
       <tr>
         <td>${categoryValue}</td>
         <td>${amountValue}</td>
         <td>${dateValue}</td>
-        <td><button onclick="deleteExpense(${index});  location.reload();" class="delete-btn">Delete</button></td>
+        <td><button onclick="deleteExpense(${expenses.length - 1});" class="delete-btn">Delete</button></td>
       </tr>`;
-    table.querySelector('tbody').innerHTML += html;
-    total.textContent = `Total : ${totalAmount} $`;
+    table.innerHTML += html;
+
+    sumExpenses();
   });
+}
+
+function displayExpenses() {
+  table.innerHTML = ` 
+  <table class="table-list">
+          <tr>
+            <th>Category</th>
+            <th>Amount</th>
+            <th>Date</th>
+            <th>Delete</th>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td id="td-delete"></td>
+          </tr>
+        </table>`;
+  expenses.forEach((expense, index) => {
+    const { categoryValue, amountValue, dateValue } = expense;
+    const html = `
+      <tr>
+        <td>${categoryValue}</td>
+        <td>${amountValue}</td>
+        <td>${dateValue}</td>
+        <td><button onclick="deleteExpense(${index});" class="delete-btn">Delete</button></td>
+      </tr>`;
+    table.innerHTML += html;
+  });
+  sumExpenses();
 }
 
 function deleteExpense(index) {
   expenses.splice(index, 1);
   localStorage.setItem('expenses', JSON.stringify(expenses));
-
   displayExpenses();
 }
+
 function sumExpenses() {
   let totalSum = 0;
-  for (let i = 0; i < expenses.length; i++) {
-    totalSum += expenses[i].amountValue;
-  }
-
+  expenses.forEach(expense => {
+    totalSum += expense.amountValue;
+  });
   total.textContent = `Total : ${totalSum} $`;
 }
 
